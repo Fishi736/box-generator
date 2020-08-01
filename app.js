3ef1e621ba0d0ec847ee7f49ea8ba1e1f3283c0a
Fishi736 <53339462+Fishi736@users.noreply.github.com>
2020-08-01 09:34:11 +0530

    day 4

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var count = 0;
 function greater() {
+    // getting values from user
     var number1 = document.getElementById('num1').value;
     var color = document.getElementById('color').value;
 
     for (let i = 0; i < number1; i++){
         count++;
-        var btn = document.createElement("button");
+        var btn = document.createElement("button"); //creating a button
         btn.className = "btn1";
         btn.innerHTML = "new Button" + count;
         btn.style="background-color:"+color+";"
